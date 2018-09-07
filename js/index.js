cbef6f8f0106b0a85a795ff675c9202f89843eee
Afonso Ferrer <afonsoferrer@IWSs-MacBook-Pro-2.local>
2018-09-07 14:31:39 -0300

    little memory game, there still is the need to modularize some funcitons and improve the UI

---
+++ js/index.js
@@ -0,0 +1,787 @@
+const landscapingCards = [
+	{
+		image: "images/landscaping/beach.svg", 
+		cardTitle: "The Beach",
+		cardIdentifier: "beach",
+		backgroundColor: "Blue"
+	},
+	{
+		image: "images/landscaping/bridge.svg", 
+		cardTitle: "The Great Bridge",
+		cardIdentifier: "bridge",
+		backgroundColor: "Purple"
+	},
+	{
+		image: "images/landscaping/cape.svg", 
+		cardTitle: "Cherry Blossom",
+		cardIdentifier: "cherryblossom",
+		backgroundColor: "Red"
+	},
+	{
+		image: "images/landscaping/castle.svg", 
+		cardTitle: "Old Castle",
+		cardIdentifier: "castle",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/cityscape.svg", 
+		cardTitle: "Distant City",
+		cardIdentifier: "city",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/desert-1.svg", 
+		cardTitle: "Desert Mountains",
+		cardIdentifier: "desert1",
+		backgroundColor: "Yellow"
+	},
+	{
+		image: "images/landscaping/desert.svg", 
+		cardTitle: "Conquered Desert",
+		cardIdentifier: "desert2",
+		backgroundColor: "Yellow"
+	},
+	{
+		image: "images/landscaping/fields-1.svg", 
+		cardTitle: "Fields",
+		cardIdentifier: "fields1",
+		backgroundColor: "Yellow"
+	},
+	{
+		image: "images/landscaping/fields.svg", 
+		cardTitle: "Flower Fields",
+		cardIdentifier: "fields2",
+		backgroundColor: "Blue"
+	},
+	{
+		image: "images/landscaping/forest.svg", 
+		cardTitle: "The Forest",
+		cardIdentifier: "forest1",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/hills.svg", 
+		cardTitle: "Forest Hills",
+		cardIdentifier: "forest2",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/home-1.svg", 
+		cardTitle: "Sunny House",
+		cardIdentifier: "house",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/home.svg", 
+		cardTitle: "Distant Home",
+		cardIdentifier: "house",
+		backgroundColor: "Yellow"
+	},
+	{
+		image: "images/landscaping/iceberg.svg", 
+		cardTitle: "Iceberg",
+		cardIdentifier: "iceberg",
+		backgroundColor: "Blue"
+	},
+	{
+		image: "images/landscaping/island.svg", 
+		cardTitle: "The Island",
+		cardIdentifier: "island",
+		backgroundColor: "Blue"
+	},
+	{
+		image: "images/landscaping/mill.svg", 
+		cardTitle: "The Old Mill",
+		cardIdentifier: "mill",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/mountains-1.svg", 
+		cardTitle: "Snow Mountains",
+		cardIdentifier: "mountains1",
+		backgroundColor: "Blue"
+	},
+	{
+		image: "images/landscaping/mountains.svg", 
+		cardTitle: "Rising Moon",
+		cardIdentifier: "mountains2",
+		backgroundColor: "Purple"
+	},
+	{
+		image: "images/landscaping/nuclear-plant.svg", 
+		cardTitle: "Future",
+		cardIdentifier: "future1",
+		backgroundColor: "Yellow"
+	},
+	{
+		image: "images/landscaping/river.svg", 
+		cardTitle: "Flowing River",
+		cardIdentifier: "river",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/ruins.svg", 
+		cardTitle: "The Ruins",
+		cardIdentifier: "ruins",
+		backgroundColor: "Yellow"
+	},
+	{
+		image: "images/landscaping/sea.svg", 
+		cardTitle: "Great Sea",
+		cardIdentifier: "sea",
+		backgroundColor: "Red"
+	},
+	{
+		image: "images/landscaping/spruce.svg", 
+		cardTitle: "Hiding Forest",
+		cardIdentifier: "forest3",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/trees.svg", 
+		cardTitle: "Trees",
+		cardIdentifier: "forest4",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/village.svg", 
+		cardTitle: "Village",
+		cardIdentifier: "village",
+		backgroundColor: "Yellow"
+	},
+	{
+		image: "images/landscaping/waterfall-1.svg", 
+		cardTitle: "Great Waterfall",
+		cardIdentifier: "waterfall1",
+		backgroundColor: "Blue"
+	},
+	{
+		image: "images/landscaping/waterfall.svg", 
+		cardTitle: "Watterfall",
+		cardIdentifier: "waterfall2",
+		backgroundColor: "Green"
+	},
+	{
+		image: "images/landscaping/windmills.svg", 
+		cardTitle: "New Future",
+		cardIdentifier: "future2",
+		backgroundColor: "Green"
+	}
+];
+
+const educationCards = [
+	{
+		image: "images/education/abacus.svg", 
+		cardTitle: "Abacus",
+		cardIdentifier: "abacus",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/alarm-clock.svg", 
+		cardTitle: "Alarm-clock",
+		cardIdentifier: "alarm-clock",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/american-football.svg", 
+		cardTitle: "American-football",
+		cardIdentifier: "american-football",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/apple.svg", 
+		cardTitle: "Apple",
+		cardIdentifier: "apple",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/backpack.svg", 
+		cardTitle: "Backpack",
+		cardIdentifier: "backpack",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/basketball.svg", 
+		cardTitle: "Basketball",
+		cardIdentifier: "basketball",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/bell.svg", 
+		cardTitle: "Bell",
+		cardIdentifier: "bell",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/blackboard-1.svg", 
+		cardTitle: "Blackboard-1",
+		cardIdentifier: "blackboard-1",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/blackboard.svg", 
+		cardTitle: "Blackboard",
+		cardIdentifier: "blackboard",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/books-1.svg", 
+		cardTitle: "Books-1",
+		cardIdentifier: "books-1",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/books.svg", 
+		cardTitle: "Books",
+		cardIdentifier: "books",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/bookshelf.svg", 
+		cardTitle: "Bookshelf",
+		cardIdentifier: "bookshelf",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/briefcase.svg", 
+		cardTitle: "Briefcase",
+		cardIdentifier: "briefcase",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/column.svg", 
+		cardTitle: "Column",
+		cardIdentifier: "column",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/compass.svg", 
+		cardTitle: "Compass",
+		cardIdentifier: "compass",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/computer-mouse.svg", 
+		cardTitle: "Computer-mouse",
+		cardIdentifier: "computer-mouse",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/desk.svg", 
+		cardTitle: "Desk",
+		cardIdentifier: "desk",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/diploma-1.svg", 
+		cardTitle: "Diploma-1",
+		cardIdentifier: "diploma-1",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/diploma.svg", 
+		cardTitle: "Diploma",
+		cardIdentifier: "diploma",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/earth-globe.svg", 
+		cardTitle: "Earth-globe",
+		cardIdentifier: "earth-globe",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/eraser.svg", 
+		cardTitle: "Eraser",
+		cardIdentifier: "eraser",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/flask.svg", 
+		cardTitle: "Flask",
+		cardIdentifier: "flask",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/fountain-pen.svg", 
+		cardTitle: "Fountain-pen",
+		cardIdentifier: "fountain-pen",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/glasses.svg", 
+		cardTitle: "Glasses",
+		cardIdentifier: "glasses",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/light-bulb.svg", 
+		cardTitle: "Light-bulb",
+		cardIdentifier: "light-bulb",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/medal.svg", 
+		cardTitle: "Medal",
+		cardIdentifier: "medal",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/microscope.svg", 
+		cardTitle: "Microscope",
+		cardIdentifier: "microscope",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/molecule.svg", 
+		cardTitle: "Molecule",
+		cardIdentifier: "molecule",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/mortarboard.svg", 
+		cardTitle: "Mortarboard",
+		cardIdentifier: "mortarboard",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/newtons-cradle.svg", 
+		cardTitle: "Newtons-cradle",
+		cardIdentifier: "newtons-cradle",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/notebook.svg", 
+		cardTitle: "Notebook",
+		cardIdentifier: "notebook",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/open-book.svg", 
+		cardTitle: "Open-book",
+		cardIdentifier: "open-book",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/owl.svg", 
+		cardTitle: "Owl",
+		cardIdentifier: "owl",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/paint-brush.svg", 
+		cardTitle: "Paint-brush",
+		cardIdentifier: "paint-brush",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/palette.svg", 
+		cardTitle: "Palette",
+		cardIdentifier: "palette",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/paperclip.svg", 
+		cardTitle: "Paperclip",
+		cardIdentifier: "paperclip",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/pen.svg", 
+		cardTitle: "Pen",
+		cardIdentifier: "pen",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/pencil.svg", 
+		cardTitle: "Pencil",
+		cardIdentifier: "pencil",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/physics.svg", 
+		cardTitle: "Physics",
+		cardIdentifier: "physics",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/professor.svg", 
+		cardTitle: "Professor",
+		cardIdentifier: "professor",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/school-bus.svg", 
+		cardTitle: "School-bus",
+		cardIdentifier: "school-bus",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/school.svg", 
+		cardTitle: "School",
+		cardIdentifier: "school",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/scissors.svg", 
+		cardTitle: "Scissors",
+		cardIdentifier: "scissors",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/set-square.svg", 
+		cardTitle: "Set-square",
+		cardIdentifier: "set-square",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/student.svg", 
+		cardTitle: "Student",
+		cardIdentifier: "student",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/test-tubes.svg", 
+		cardTitle: "Test-tubes",
+		cardIdentifier: "test-tubes",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/test.svg", 
+		cardTitle: "Test",
+		cardIdentifier: "test",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/university.svg", 
+		cardTitle: "University",
+		cardIdentifier: "university",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/violin.svg", 
+		cardTitle: "Violin",
+		cardIdentifier: "violin",
+		backgroundColor: "gray"
+	},
+	{
+		image: "images/education/whistle.svg", 
+		cardTitle: "Whistle",
+		cardIdentifier: "whistle",
+		backgroundColor: "gray"
+	}
+];
+
+const landscapingDeck = {
+	deckSize: 28,
+	deckFrontStyle: "",
+	cards: landscapingCards
+}
+
+const educationDeck = {
+	deckSize: 50,
+	deckFrontStyle: "",
+	cards: educationCards
+}
+
+const gameProperties = {
+	level: {
+		easy: {
+			deckSize: 6
+		},
+
+		regular: {
+			deckSize: 10
+		},
+
+		hard: {
+			deckSize: 15
+		}
+	}
+}
+
+var gameData = {
+	// @TODO change the card to an array of current cards, from 2 to n
+	card1: null,
+	card2: null,
+	mistakes: 0,
+	cardsFlipped: 0,
+	remainingCards: -1
+} 
+
+function initializeGame(){
+	let form = document.getElementById("gamePropertiesForm");
+	let gameParameters = parseForm(form);
+	createGameDeck(gameParameters);
+}
+
+function parseForm(form){
+	let gameParameters = {};
+	switch (form.elements.level.value){
+		case "regular":
+			gameParameters.level = 10;
+			break;
+		case "easy":
+			gameParameters.level = 6;
+			break;
+		case "hard":
+			gameParameters.level = 15;
+			break;
+		default:
+			gameParameters.level = 10;
+	}
+
+	switch(form.elements.deck.value){
+		case "landscaping":
+			gameParameters.deck = landscapingDeck;
+			break;
+		case "education":
+			gameParameters.deck = educationDeck;
+			break;
+	}
+	gameParameters.mode = form.elements.mode.value;
+	return gameParameters;
+}
+
+function nextGameState(card){
+	// @TODO more generic logic here
+	if (gameData.card1 == null) {
+		gameData.card1 = card;
+	} else {
+		gameData.card2 = card;
+		if (areCurrentCardsTheSame()){
+			decreaseRemainingCards();
+			updateCorrectCardsChosen();
+		} else {
+			increaseGameMistakes()
+			updateWrongCardsChosen();
+		}
+	}
+}
+
+function decreaseRemainingCards(){
+
+	gameData.remainingCards = gameData.remainingCards-2;
+}
+
+function increaseGameMistakes(){
+
+	gameData.mistakes++;
+}
+
+function getCardIdentifier(card){
+
+	return card.dataset.cardidentifier;
+}
+
+function areCurrentCardsTheSame (){
+	//@TODO more generic logic here
+	let cardIdentifier1 = getCardIdentifier(gameData.card1);
+	    cardIdentifier2 = getCardIdentifier(gameData.card2);
+	if (cardIdentifier1 === cardIdentifier2){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function getCardImage(card){
+
+	return card.lastChild;
+}
+
+function updateCorrectCardsChosen(){
+	// @TODO more generic logic here
+	let card1Image = getCardImage(gameData.card1);
+		card2Image = getCardImage(gameData.card2);
+	setTimeout(updateBackFaceStyle.bind(null, card1Image, card2Image), 1000);
+	disableCurrentCards();
+	removeCurrentCardsInfo();
+}
+
+function disableCurrentCards(){
+	// @TODO more generic logic here
+	gameData.card1.removeEventListener("click", updateGame);
+	gameData.card2.removeEventListener("click", updateGame);
+}
+
+function updateBackFaceStyle(cardBackFace1, cardBackFace2){
+	// @TODO more generic logic here
+	cardBackFace1.classList.add("card__face--disabled");
+	cardBackFace2.classList.add("card__face--disabled");
+}
+
+function updateWrongCardsChosen(){
+	// @TODO more generic logic here
+	setTimeout(unflipCurrentCards.bind(null, gameData.card1, gameData.card2), 1000);
+	removeCurrentCardsInfo();
+}
+
+function unflipCurrentCards(card1, card2){
+	// @TODO more generic logic here
+	card1.classList.toggle('flippedCard');
+	card2.classList.toggle('flippedCard');
+}
+
+function removeCurrentCardsInfo(){
+	// @TODO more generic logic here
+	gameData.card1 = null;
+	gameData.card2 = null;
+}
+
+function updateGame(){
+	if (!this.classList.contains("flippedCard")) {
+    	this.classList.toggle('flippedCard');
+    	gameData.cardsFlipped++;
+    	nextGameState(this);
+	}
+}
+
+function createCard(cardProperties){
+	let card = document.createElement("DIV");
+	card.classList.add("card");	
+	card.setAttribute("data-cardidentifier",cardProperties.cardIdentifier);
+
+	card.addEventListener( 'click', updateGame);  
+	
+	let cardFront = createFrontFace();
+	card.appendChild(cardFront);
+	
+	let cardBack = createBackFace(cardProperties);
+	card.appendChild(cardBack);
+	
+	return card;
+}
+
+function createFrontFace(){
+	let frontFace = document.createElement("DIV");
+	frontFace.classList.add("card__face");
+	frontFace.classList.add("card__face--front");
+	frontFace.classList.add("card__face--backgroundWorld");
+	frontFace.classList.add("card__face--backgroundGray");
+	return frontFace;
+}
+
+function createBackFace(cardProperties){
+	let backFace = document.createElement("DIV");
+
+	backFace.classList.add("card__face");
+	backFace.classList.add("card__face--back");
+	let backgroundColor = "card__face--background" + cardProperties.backgroundColor;
+	backFace.classList.add(backgroundColor)
+	backFace.appendChild(createBackFaceImage(cardProperties));
+
+	let cardTitle = document.createElement("P");
+	let titleColor = "card__title--" + cardProperties.backgroundColor;
+	cardTitle.classList.add("card__title");
+	cardTitle.classList.add(titleColor);
+	cardTitle.textContent = cardProperties.cardTitle;
+
+	backFace.appendChild(cardTitle);
+	return backFace;
+}
+
+function createBackFaceImage(cardProperties){
+	let backFaceImage = document.createElement("IMG");
+	backFaceImage.src=cardProperties.image;
+	return backFaceImage;
+}
+
+function getDOMDeck(){
+	let DOMdeck = document.getElementById("cardDeck");
+
+	if(DOMdeck == undefined){
+		return createDOMDeck();	
+	} else {
+		return DOMdeck;
+	}
+}
+
+function createDOMDeck(){
+	let DOMTable = document.getElementById("cardTable");
+	let DOMdeck = document.createElement("DIV");
+	DOMdeck.classList.add("deck");
+	DOMdeck.id = "cardDeck";	
+	DOMTable.appendChild(DOMdeck);
+	return DOMdeck;
+}
+
+function createGameDeck(gameProperties){
+	let deck = gameProperties.deck,
+	    gameDeckSize = gameProperties.level;
+
+	let DOMdeck = getDOMDeck();
+
+	if (gameDeckSize == undefined || gameDeckSize > deck.deckSize){
+		gameDeckSize = gameProperties.level.regular.deckSize;
+	}
+
+	let cardsStyles = generatePositionsArray(deck.deckSize);
+	cardsStyles = shuffle(cardsStyles);
+	let gameDeck = [];
+	let j = 0;
+	for (let i = 0; i < gameDeckSize; i++){
+		let card = createCard(deck.cards[cardsStyles[j]]);
+		let cardTwin = createCard(deck.cards[cardsStyles[j++]]);
+		gameDeck.push(card, cardTwin);
+	}
+
+	gameDeck = shuffle(gameDeck);
+
+	for (let i=0; i <gameDeck.length; i++){
+		DOMdeck.appendChild(gameDeck[i]);
+	}
+	gameData.remainingCards = gameDeck.length;
+}
+
+function removeDeck(){
+	let DOMdeck = document.getElementById("cardDeck");
+	DOMdeck.remove();	
+}
+
+function generatePositionsArray(size){
+	let positionsArray = new Array(size);
+	for (let i = 0; i < size; i++){
+		positionsArray[i] = i;
+	}
+	return positionsArray;
+}
+
+function shuffle(array){
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+function isCardDisabled(card){
+	if (card == undefined){
+		return undefined;
+	} else {
+		return card.lastChild.classList.contains("card__face--disabled");
+	}
+}
+
+function isCardPlayerFlipped(card){
+	if (card == undefined){
+		return undefined;
+	} else {
+		return (card.classList.contains("flippedCard") && (card === gameData.card1));
+	}
+}
+
+function _flipAllAvailableCards(){
+	var cards = document.getElementsByClassName("card");
+	for (var i = 0; i < cards.length; i++) {
+		if (!isCardDisabled(cards[i]) && !isCardPlayerFlipped(cards[i])){
+		  cards[i].classList.toggle('flippedCard');	
+		}
+	}
+	removeCurrentCardsInfo();
+}
